601643b53e2c4b665778c7260fdbc7cfef03bc73
DJ Tarazona <me@djtarazona.com>
2014-07-22 13:51:53 -0700

    Disable logging

--- lelib/lelib.h
+++ lelib/lelib.h
@@ -6,13 +6,7 @@
 //  Copyright (c) 2013,2014 Logentries. All rights reserved.
 //
 
-#ifndef LE_DEBUG_LOGS
-    #ifdef DEBUG
-        #define LE_DEBUG_LOGS 1
-    #else
-        #define LE_DEBUG_LOGS 0
-    #endif
-#endif
+#define LE_DEBUG_LOGS 0
 
 #if LE_DEBUG_LOGS
 #define LE_DEBUG(...)         NSLog(__VA_ARGS__)
